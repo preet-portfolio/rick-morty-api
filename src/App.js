2d556789ede4abe68820b8311a986b9f7fa71e20
Preet Panchal <preet2020@icloud.com>
2023-03-01 00:10:39 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  // console.log(results);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  //useEffect
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        I'm Not a Robot, Goddammit Morty,
+        <span className="text-primary">I'm a API!</span>
+      </h1>
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+      <div className="container">
+        <div className="row">
+          <Filters />
+
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
+  // console.log(info.pages);
+  return (
+    <ReactPaginate
+      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+      className="pagination justify-content-center gap-4 my-4"
+      nextLabel="Next"
+      previousLabel="Prev"
+      nextClassName="btn btn-primary"
+      previousClassName="btn btn-primary"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      activeClassName="active"
+      onPageChange={(data) => {
+        setPageNumber(data.selected + 1);
+      }}
+      pageCount={info?.pages}
+    />
+  );
+};
+
+export default Pagination;
